eab686e93e2f223f594eeda6d85d80e1b2dbd682
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2022-05-20 22:30:14 -0300

    Initial commit

---
+++ src/EntradadeDados2.java
@@ -0,0 +1,26 @@
+
+import  java.util.Scanner ;
+public class EntradadeDados2 {
+
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(  System .in);
+        String   x ;
+        x = sc.next();
+
+
+
+
+
+
+        System.out.println( " voce digitou   : " + x);
+
+
+        sc.close();
+
+
+
+
+
+
+    }
+}
